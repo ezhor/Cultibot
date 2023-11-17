401f41774e6db9c9ddd72b1698ae6ce5ed5d0755
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-11-17 15:21:44 +0100

    Infinite polling loop just in case (internet connection issues, etc)

--- telegram-bot.py
+++ telegram-bot.py
@@ -2,6 +2,8 @@ import configparser
 from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+import urllib.request
+
 
 config = configparser.ConfigParser()
 config.read("../config.txt")
@@ -16,15 +18,16 @@ async def picture(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     picturesList.sort()
     lastPicture = picturesList[-1]
     imagePath = f"{imagesParentDirectory}/{lastDay}/{lastPicture}"
-    print(imagePath)
     await update.message.reply_photo(photo = imagePath, caption = f"#picture\n{lastDay}\n{lastPicture[0:2]}:{lastPicture[2:4]}")
 
 async def timelapse(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Not implemented yet :(")
 
-app = ApplicationBuilder().token(token).build()
-
-app.add_handler(CommandHandler("picture", picture))
-app.add_handler(CommandHandler("timelapse", timelapse))
-
-app.run_polling()
+while True:
+    try:
+        app = ApplicationBuilder().token(token).build()
+        app.add_handler(CommandHandler("picture", picture))
+        app.run_polling()
+    except Exception as e:
+        print(e)
+        pass
