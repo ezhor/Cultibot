8bb27323cc54c4f66a6930248745de40db001368
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-11-17 15:57:47 +0100

    Presumably fixed async errors.

--- telegram-bot.py
+++ telegram-bot.py
@@ -20,15 +20,13 @@ async def picture(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     imagePath = f"{imagesParentDirectory}/{lastDay}/{lastPicture}"
     await update.message.reply_photo(photo = imagePath, caption = f"#picture\n{lastDay}\n{lastPicture[0:2]}:{lastPicture[2:4]}")
 
-async def timelapse(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Not implemented yet :(")
-
 while True:
     try:
-        app = ApplicationBuilder().token(token).build()
-        app.add_handler(CommandHandler("picture", picture))
-        app.run_polling()
+        app = ApplicationBuilder().token(token).build()        
     except Exception as e:
         print(e)
         time.sleep(1)
         pass
+
+app.add_handler(CommandHandler("picture", picture))
+app.run_polling()
