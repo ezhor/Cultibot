59b6364c64b956be9e1e79882576f512313866ca
Ézhor Malkávar <ezhormalkavar@gmail.com>
2025-05-13 22:30:24 +0200

    Current soil moisture request via serial

---
+++ moisture.py
@@ -0,0 +1,15 @@
+import serial, time
+
+def currentMoistureRawLevel():
+        arduino = serial.Serial("COM3", 9600, timeout=10)
+        time.sleep(5)
+        arduino.reset_input_buffer()
+        arduino.reset_output_buffer()
+        arduino.write("?\n".encode())
+        arduino.readline() # Ignore echo data
+        data = int(arduino.readline())
+        arduino.close()
+        return data
+
+if __name__ == "__main__":
+    print(str(currentMoistureRawLevel()))
