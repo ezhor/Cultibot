3eaf1315e3fd512c7d96df09622aa4af88fd0f6c
Ézhor Malkávar <ezhormalkavar@gmail.com>
2025-05-14 13:24:02 +0200

    Moisture value normalization

--- moisture.py
+++ moisture.py
@@ -1,6 +1,6 @@
-import serial, time
+import serial, time, configparser
 
-def currentMoistureRaw():
+def currentMoistureRaw() -> int:
         arduino = serial.Serial("/dev/ttyUSB0", 9600, timeout=10)
         time.sleep(5)
         arduino.reset_input_buffer()
@@ -11,5 +11,16 @@ def currentMoistureRaw():
         arduino.close()
         return data
 
+def rawToNormalized(rawValue: int) -> int:
+        config = configparser.ConfigParser()
+        config.read("../config.txt")
+        min = int(config["Moisture"]["min"])
+        max = int(config["Moisture"]["max"])
+        maxDelta = min - max
+        valueDelta = rawValue - max
+        normalizedDelta = round(100 - (100 * valueDelta / maxDelta))
+        return normalizedDelta
+
+
 if __name__ == "__main__":
-    print(str(currentMoistureRaw()))
+    print(str(rawToNormalized(currentMoistureRaw())) + "%")
